aee11df01a6537f0269dc0666f5ae27dbda03def
JavohirAkramov <61544715+JavohirAkramov@users.noreply.github.com>
2021-12-09 16:30:55 +0500

    Add files via upload

---
+++ TodoList2/src/App.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import TodoList from "./components/TodoList/TodoList";
+
+function App(props) {
+  return (
+    <div>
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
